36f4a90cc83fa60d7ce3f2ea001d3662136e6d2e
Fran3478 <franciscohv95@gmail.com>
2022-02-10 16:02:57 -0300

    Modificacion del js, crea listado de los nombres que recibe

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const url="https://rickandmortyapi.com/api/character";
+
 function getData(){
-    fetch("https://rickandmortyapi.com/api/character")
+    fetch(url)
         .then(response => response.json())
         .then(data=>{
-            
+            var name;
+            for (character of data.results){
+                var li=document.createElement("li");
+                var p=document.createElement("p");
+                name=character.name;
+                p.appendChild(document.createTextNode(name));
+                document.querySelector("#list-characters").appendChild(li).appendChild(p);
+            }
         })
 }
