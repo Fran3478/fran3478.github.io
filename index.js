d3dd458155384fefb212d135626e649fead05944
Fran3478 <franciscohv95@gmail.com>
2022-02-11 11:54:18 -0300

    Mas mod graficas y se agrego la imagen a la lista de presentacion

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ function getData(){
         .then(response => response.json())
         .then(data=>{
             var name;
+            var image;
             for (character of data.results){
                 var li=document.createElement("li");
                 var p=document.createElement("p");
+                var img=document.createElement("img");
                 name=character.name;
+                img.src=character.image;
                 p.appendChild(document.createTextNode(name));
+                document.querySelector("#list-characters").appendChild(li).appendChild(img);
                 document.querySelector("#list-characters").appendChild(li).appendChild(p);
             }
         })
