acd4ace2d938099ba8b8e78078fa2665e8c30421
Fran3478 <franciscohv95@gmail.com>
2022-02-14 17:34:59 -0300

    integracion completamente funcional del modal

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ function getData(){
                 var li=document.createElement("li");
                 var btn=document.createElement("button");
                 name=character.name;
+                urlImg=character.image;
+                species=character.species;
                 btn.className="btn";
                 btn.type="button";
                 btn.setAttribute("data-bs-toggle", "modal");
                 btn.setAttribute("data-bs-target", "#exampleModal");
+                btn.setAttribute("char-name", name);
+                btn.setAttribute("url-img", urlImg);
+                btn.setAttribute("char-species", species)
                 li.id="personaje";
                 btn.appendChild(document.createTextNode(name));
                 document.querySelector("#list-characters").appendChild(li).appendChild(btn);
@@ -20,6 +25,26 @@ function getData(){
         })
 }
 
-function showData(){
-    console.log("click");
-}
+var exampleModal = document.getElementById('exampleModal')
+console.log("ok");
+exampleModal.addEventListener('show.bs.modal', function (event) {var button = event.relatedTarget
+    var name = button.getAttribute('char-name')
+    var urlImg = button.getAttribute('url-img')
+    var species = button.getAttribute('char-species')
+    var modalTitle = exampleModal.querySelector('.modal-title')
+    var div=document.createElement("div");
+    var img=document.createElement("img");
+    var p=document.createElement("p");
+    div.className="div-img";
+    img.src=urlImg;
+    img.className="img-char";
+    p.appendChild(document.createTextNode(species))
+    p.className="spieces";
+    modalTitle.textContent = name
+    document.querySelector("#modalBody").appendChild(div).appendChild(img);
+    document.querySelector("#modalBody").appendChild(p);
+})
+
+$('.modal').on('hidden.bs.modal', function () {
+    $(this).removeData('modal').find('.modal-body').html('')
+});
